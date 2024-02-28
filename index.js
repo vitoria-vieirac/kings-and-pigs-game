bc68eb1b915b3f6f3a1f6ba7f1b1674b057a55a4
vitoria-vieirac <114622524+vitoria-vieirac@users.noreply.github.com>
2024-02-28 18:59:03 -0300

    first commit game

---
+++ index.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 64 * 16;
+canvas.height = 64 * 9;
+
+const parsedCollisions = collisionsLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjectsFrom2D();
+
+const backgroundLevel1 = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/backgroundLevel1.png",
+});
+
+const player = new Player();
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
+
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
+
+  player.draw();
+  player.update();
+}
+
+animate();
